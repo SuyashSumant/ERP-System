2e300526504d346a2e8498d23afc8ca2d10a8a4e
SuyashSumant <suyashsumant28@gmail.com>
2025-04-12 11:50:46 +0530

    Customer page added

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.erp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.erp to javafx.fxml;
+    exports com.example.erp;
+}
